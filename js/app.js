f64f5408e629789b8f731921de8d96a63550afa4
Vita Kuznetsova <vita_kuznetsovav@gmail.com>
2022-02-16 19:14:31 +0200

    todo styles for complete

--- js/app.js
+++ js/app.js
@@ -1,81 +1,154 @@
 const dom = {
-  formTodo: document.getElementById("form-todo"),
-  todoList: document.getElementById("todo-list"), 
-}
+  formTodo: document.getElementById('form-todo'),
+  todoList: document.getElementById('todo-list'),
+};
 
-// const todos = [
-//   {
-//     title: 'Explore and Travel',
-//     completed: false
-//   },
-//   {
-//     title: 'A new way to explore the world ',
-//     completed: false
-//   },
-//   {
-//     title: 'Guides by Thousand Sunny',
-//     completed: false
-//   }
-// ]
-
+let todos = [
+  {
+    id: 1,
+    title: 'Explore and Travel',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'A new way to explore the world ',
+    completed: true,
+  },
+  {
+    id: 3,
+    title: 'Guides by Thousand Sunny',
+    completed: false,
+  },
+];
 dom.formTodo.addEventListener('submit', (e) => {
-  e.preventDefault()
   const inputValue = e.target.title.value;
-  let newTodo = {
+  const newTodo = {
     title: inputValue,
-    completed: false
+    completed: false,
+    id: Date.now()
   }
-  // fetchData('./data/data.json', 'POST', newTodo)
   e.target.reset();
 })
 
-dom.todoList.addEventListener('click', (e) => {
-  if(e.target.closest('.btn--cancel')) {
-    // fetchData('./data/dat.json', 'DELETE')
-  }
-})
+renderTodos(todos, dom.todoList)
 
-//вставити текст
-function createTodo(todo) {
+function renderTodos(todos, todoList) {
+  todoList.innerHTML = createTodo(todos)
+}
+
+function createTodo(todos) {
+  return todos.map(todo => createTodoHTML(todo))
+}
+
+function createTodoHTML(todo) {
   return `
-  <li class="todo__item todo-item">
-      <p>${todo.title}</p>
-      <button class="btn btn--cancel"">
-          <img src="./img/cancel-icon.svg" class="img-cancel" alt="Delete" width="1" height="1">
-      </button>
-  </li>
+      <li class="todo__item todo-item">
+      <p class="${todo.completed ? 'todo-item--completed' : ''}">${todo.title}</p>
+      <div class="todo-btn"> 
+      ${ !todo.completed
+         ? ` <button class="btn btn--icon">
+                <img src="./img/copmlete.png" class="btn-img" alt="Delete" width="1" height="1">
+              </button>`  : ''
+        }
+        <button class="btn btn--icon">
+            <img src="./img/cancel-icon.svg" class="btn-img" alt="Delete" width="1" height="1">
+        </button>
+      </div>
+    </li>
   `
 }
 
-//розєднати туду
-function renderTodoHTML(todos) {
-  return todos.map(todo => createTodo(todo))
-}
+// renderTodos(todos, dom.todoList);
 
-//вставити туду в список
-function renderTodos(todos, todoList) {
-  todoList.innerHTML = renderTodoHTML(todos)
-}
+// dom.formTodo.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const inputValue = e.target.title.value.trim();
+//   const newTodo = {
+//     title: inputValue,
+//     completed: false,
+//     id: Date.now(),
+//   };
+//   todos.push(newTodo);
+//   renderTodos(todos, dom.todoList);
+//   // fetchData('./data/data.json', 'POST', newTodo)
+//   e.target.reset();
+// });
 
-async function fetchData(url, method = 'GET', data) {
-  try {
-    let response = await fetch(url, {
-      method,
-      body: data && JSON.stringify(data),
-      headers: {
-        "content-type": "application/json;charset=utf-8"
-      }
-    })
-    if(response.ok) {
-      let data = await response.json();
-      renderTodos(data, dom.todoList)
-    }
-  } catch (error) {
-    console.warn(error)
-  }
-}
+// dom.todoList.addEventListener('click', (e) => {
+//   const deleteBtn = e.target.closest('.btn--cancel');
+//   const completeBtn = e.target.closest('.btn--complete');
+//   if (deleteBtn || completeBtn) {
+//     const todoItem = e.target.closest('.todo-item');
+//     const todoId = Number(todoItem.dataset.id);
+//     if (deleteBtn) {
+//       console.log('DELETE!!!', todoId);
+//       // const todoIdx = todos.findIndex(todo => todo.id === todoId)
+//       // todos.splice(todoIdx, 1)
+//       //or
+//       todos = todos.filter((todo) => todo.id !== todoId);
+//     }
+//     if (completeBtn) {
+//       console.log('EDIT!!!', todoId);
+//       const todo = todos.find((todo) => todo.id === todoId);
+//       todo.completed = true;
 
-fetchData('./data/data.json');
+//       // const todoIdx = todos.findIndex(todo => todo.id === todoId)
+//       // const todo = {...todos[todoIdx]}
+//       // todo.completed = true
+//       // todos.splice(todoIdx, 1, todo)
+//     }
+//     renderTodos(todos, dom.todoList);
+//   }
+// });
+
+// //вставити текст
+// function createTodo(todo) {
+//   return `
+  // <li class="todo__item todo-item" data-id="${todo.id}">
+  //     <p class="${todo.completed ? 'todo-item--completed' : ''}">${todo.title}</p>
+  //     ${
+  //       !todo.completed
+  //         ? `<button class="btn btn--complete">
+  //               Complete
+  //           </button>`
+  //         : ''
+  //     }
+  //     <button class="btn btn--cancel">
+  //         <img src="./img/cancel-icon.svg" class="img-cancel" alt="Delete" width="1" height="1">
+  //     </button>
+  // </li>
+//   `;
+// }
+
+// //розєднати туду
+// function renderTodoHTML(todos) {
+//   return todos.map((todo) => createTodo(todo)).join('');
+// }
+
+// //вставити туду в список
+// function renderTodos(todos, todoList) {
+//   todoList.innerHTML = renderTodoHTML(todos);
+// }
+
+// async function fetchData(url, method = 'GET', data) {
+//   try {
+//     let response = await fetch(url, {
+//       method,
+//       body: data && JSON.stringify(data),
+//       headers: {
+//         'content-type': 'application/json;charset=utf-8',
+//       },
+//     });
+//     if (response.ok) {
+//       let data = await response.json();
+//       renderTodos(data, dom.todoList);
+//     }
+//   } catch (error) {
+//     console.warn(error);
+//   }
+// }
+
+// fetchData('./data/data.json');
 
 // async function fetchData(url) {
 //   try {
